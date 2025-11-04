ecd65ba41b9473d5c4489e40f26bdcb946711a98
boss683 <adamhuseinhussein27@gmail.com>
2025-11-04 16:37:40 +0200

    my chat take

---
+++ chat.js
@@ -0,0 +1,20 @@
+// document.write('your worke not good pleces go back aginn')
+// console.log('red')
+// // window.alert('holl wored')
+// document.querySelector('h1').style.color='red';
+// document.write('<h3>supporte orguniztion mahama camp refgaerr Rwanda</h3>')
+
+// document.write('<p>partenes</p>');
+
+// document.write('<p>partenes</p>');
+
+// document.write("partenes");
+// console.log ( 45 );
+let unm= 10 ;
+let unm1 = 13 ;
+
+unm ;
+console.log(unm++)
+console.log('6');
+console.log(Number('5'));
+

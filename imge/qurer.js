972cab88e680157dbdc0aab0f9ca14da6d668ebd
boss683 <adamhuseinhussein27@gmail.com>
2025-11-16 08:57:43 +0200

    this worke chat

---
+++ imge/qurer.js
@@ -0,0 +1,29 @@
+// let but = document.querySelector('#but');
+
+// let butoff = document.querySelector('#butoff');
+
+// but.addEventListener('click', () => {
+//     let image = document.querySelector('img');
+
+//     image.src = "./derk.png"
+// })
+
+//  butoff.addEventListener('click', () => {
+//     let image = document.querySelector('img');
+
+//    image.src = "/imge/Girls Space (1) (1) (2).png"
+// })
+//let han =document.getElementsByClassName('amal');
+
+
+   const but1 = document.getElementById("but");
+   but1.addEventListener("click",()=>{
+    let paragraph=document.querySelector("#t");
+    paragraph.innerHTML="button is clicked";
+   })  
+
+
+
+
+
+
